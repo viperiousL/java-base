8725d7a0a84df29a99fba1d66ba884ab91109b76
libing <1275193680@qq.com>
2024-07-19 20:56:13 +0800

    解决乱码问题

--- heima/day01/src/com/itheima/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package day01.src.com.itheima;
-
-public class HelloWorld{
-	public static void main(String[] args){
-		System.out.println("HelloWorld");
-	}
-} 
---
+++ itheima/day02/src/com/itheima/scanner/ScannerDemo2.java
@@ -0,0 +1,15 @@
+package com.itheima.scanner;
+
+import java.util.Scanner;
+
+public class ScannerDemo2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入第一个数字");
+        int i = scanner.nextInt();
+        System.out.println("请输入第二个数字");
+        int j = scanner.nextInt();
+        System.out.println(i + j);
+    }
+}
