c2a0a37b1c59031d298c634168d2cb1f29f01d00
libing <1275193680@qq.com>
2024-07-19 20:34:29 +0800

    修改包路径

--- heima/day02/src/com/itheima/scanner/ScannerDemo1.java
+++ heima/day02/src/com/itheima/scanner/ScannerDemo1.java
@@ -2,7 +2,7 @@ package com.itheima.scanner;
 
 import java.util.Scanner;
 
-public class ScannerTest {
+public class ScannerDemo1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
